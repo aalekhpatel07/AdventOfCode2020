c6e3a1b3c916866674dffca653ef7a13460db6ea
aalekhpatel07 <aalekh.gwpeck.7998@gmail.com>
2020-12-01 06:28:25 -0600

    Return code 0 instead of 1 from pylint.

--- clean.sh
+++ clean.sh
@@ -1,27 +1,32 @@
 
-for fname in driver.py template.py ./solutions
-    do
-        echo "Linting $fname with autopep8."
+function cleaner(){
+    for fname in driver.py template.py ./solutions
+        do
+            echo "Linting $fname with autopep8."
 
-        # Run autopep8 to lint it.
-        if [[ -d $fname ]]; then
-            autopep8 $fname --in-place --recursive --aggressive --aggressive --aggressive
-        else
-            autopep8 $fname --in-place --aggressive --aggressive --aggressive
-        fi
+            # Run autopep8 to lint it.
+            if [[ -d $fname ]]; then
+                autopep8 $fname --in-place --recursive --aggressive --aggressive --aggressive
+            else
+                autopep8 $fname --in-place --aggressive --aggressive --aggressive
+            fi
 
-        echo "Finished linting $fname with autopep8."
-        echo ""
-        echo "Linting $fname with black."
+            echo "Finished linting $fname with autopep8."
+            echo ""
+            echo "Linting $fname with black."
 
-        # Run black to lint it yet again.
-        black $fname
+            # Run black to lint it yet again.
+            black $fname
 
-        echo "Finished linting $fname with black."
-        echo ""
-        echo "Evaluating code quality of $fname with pylint."
-        echo ""
-        # Run pylint to evluate final code rating.
-        pylint $fname
+            echo "Finished linting $fname with black."
+            echo ""
+            echo "Evaluating code quality of $fname with pylint."
+            echo ""
 
-    done
+            # Run pylint to evluate final code rating.
+            pylint $fname
+        done
+    return 0
+}
+
+cleaner
