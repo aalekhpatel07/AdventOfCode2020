93fe423df0668f0b8718851e507df4164ebfd579
aalekhpatel07 <aalekh.gwpeck.7998@gmail.com>
2020-12-05 00:43:21 -0600

    Add initial template for Day 6.

---
+++ solutions/day_6_a.py
@@ -0,0 +1,49 @@
+"""
+My solution for the Problem i on
+Day j of Advent of Code 2020.
+This is a template that I'll be using
+to solve problems.
+"""
+
+
+def solve():
+    """
+    Replace this with a nice docstring
+    that describes what this function is supposed
+    to do.
+
+    :return: The answer required.
+    """
+    return -1
+
+
+def driver():
+    """
+    Make sure this driver returns the result.
+    :return: result - Result of computation.
+
+    Example
+    ______
+
+    _n = int(input())
+    arr = []
+    for _ in range(_n):
+        arr.append(int(input()))
+    result = solve(arr)
+
+    """
+    result = "This is a template!"
+    print(result)
+    return result
+
+
+def main():
+    """
+    Carry forward the output of driver.
+    :return: The output of driver.
+    """
+    return driver()
+
+
+if __name__ == "__main__":
+    main()
