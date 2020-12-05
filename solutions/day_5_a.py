09e4d8bb0b73a8152199173e069f5210923dbf48
aalekhpatel07 <aalekh.gwpeck.7998@gmail.com>
2020-12-04 23:42:01 -0600

    Add solution, and tests for Day 5 of AoC 2020.

--- solutions/day_5_a.py
+++ solutions/day_5_a.py
@@ -1,39 +1,56 @@
 """
-My solution for the Problem i on
-Day j of Advent of Code 2020.
-This is a template that I'll be using
-to solve problems.
+My solution for the Problem 1 on
+Day 5 of Advent of Code 2020.
 """
 
 
-def solve():
+def solve(_n, arr):
     """
-    Replace this with a nice docstring
-    that describes what this function is supposed
-    to do.
+    Given a list of list of boarding
+    passes determine the one with largest
+    id where id is given by 8 * row + col
+    where row is given by a string of 7 characters
+    and col is given by a string of 3 characters
+    ultimately forming a binary string.
 
-    :return: The answer required.
+    :param arr: The list of list of boarding passes.
+    :return: The highest id of a boarding pass.
+
     """
-    return -1
+    _i = 0
+    _best = -1
+    while _i < _n:
+        token = arr[_i]
+        _row = 0
+        _col = 0
+        for _j in range(7):
+            if token[_j] == "B":
+                _row += 2 ** (6 - _j)
+        for _z in range(3):
+            if token[7 + _z] == "R":
+                _col += 2 ** (2 - _z)
+        _idx = _row * 8 + _col
+        _best = max(_best, _idx)
+        _i += 1
+
+    _best = max(_best, _idx)
+
+    return _best
 
 
 def driver():
     """
     Make sure this driver returns the result.
     :return: result - Result of computation.
-
-    Example
-    ______
-
+    """
     _n = int(input())
     arr = []
     for _ in range(_n):
-        arr.append(int(input()))
-    result = solve(arr)
+        arr.append(input())
 
-    """
-    result = "This is a template!"
+    result = solve(_n, arr)
     print(result)
+
     return result
 
 
--- solutions/day_5_b.py
+++ solutions/day_5_b.py
@@ -1,19 +1,52 @@
 """
-My solution for the Problem i on
-Day j of Advent of Code 2020.
-This is a template that I'll be using
-to solve problems.
+My solution for the Problem 2 on
+Day 5 of Advent of Code 2020.
 """
 
 
-def solve():
+def solve(_n, arr):
     """
-    Replace this with a nice docstring
-    that describes what this function is supposed
-    to do.
+    Given a list of list of boarding
+    passes where each pass is a string
+    of 10 characters, where the first
+    7 are 'F' or 'B' and the last three
+    are 'R' or 'L', find your boarding
+    pass if most other boarding passes
+    exist and possibly some front rows
+    and some back rows may not exist.
 
+    :param arr: The list of list of boarding
+    passes.
     :return: The answer required.
     """
+
+    all_seats = {i for i in range(1024)}
+
+    existing = set()
+
+    _i = 0
+    while _i < _n:
+        token = arr[_i]
+        _row = 0
+        _col = 0
+
+        for _j in range(7):
+            if token[_j] == "B":
+                _row += 2 ** (6 - _j)
+        for _z in range(3):
+            if token[7 + _z] == "R":
+                _col += 2 ** (2 - _z)
+        _idx = _row * 8 + _col
+        existing |= {_idx}
+        _i += 1
+
+    existing |= {_idx}
+
+    cands = all_seats - existing
+
+    for elem in cands:
+        if elem - 1 not in cands and elem + 1 not in cands:
+            return elem
     return -1
 
 
@@ -21,19 +54,15 @@ def driver():
     """
     Make sure this driver returns the result.
     :return: result - Result of computation.
-
-    Example
-    ______
-
+    """
     _n = int(input())
     arr = []
     for _ in range(_n):
-        arr.append(int(input()))
-    result = solve(arr)
+        arr.append(input())
 
-    """
-    result = "This is a template!"
+    result = solve(_n, arr)
     print(result)
+
     return result
 
 
