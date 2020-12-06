794eab051af2e48eba9a0911ca4c5a0fd6785b73
aalekhpatel07 <aalekh.gwpeck.7998@gmail.com>
2020-12-06 00:38:36 -0600

    Add auto-template for map-reducible problems.

--- setup_initial_template.sh
+++ setup_initial_template.sh
@@ -1,13 +1,24 @@
 read -p "Advent of Code 2020 Day: " day
+read -p "Can these problems be map-reduced? (y/n): " mapreducible && [[ $mapreducible == [yY] || $mapreducible == [nN] ]] || exit 1
 
 read -p "Continue? (y/n): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 
+
 function copy_template(){
     if [ ! -f solutions/day_"$day"_a.py ]; then
-        cp template.py solutions/day_"$day"_a.py
+        if [$mapreducible == [nN]]; then
+            cp template.py solutions/day_"$day"_a.py
+        else
+            cp template_group.py solutions/day_"$day"_a.py
+        fi
     fi
+
     if [ ! -f solutions/day_"$day"_b.py ]; then
-        cp template.py solutions/day_"$day"_b.py
+        if [$mapreducible == [nN]]; then
+            cp template.py solutions/day_"$day"_b.py
+        else
+            cp template_group.py solutions/day_"$day"_b.py
+        fi
     fi
 }
 
