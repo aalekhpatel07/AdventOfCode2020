813d6f4ace464dab23af7a74c407de796e7ad92f
aalekhpatel07 <aalekh.gwpeck.7998@gmail.com>
2020-12-12 01:43:37 -0600

    Add solution, and tests for Day 12 of AoC 2020.

--- solutions/day_12_a.py
+++ solutions/day_12_a.py
@@ -1,9 +1,6 @@
 """
-My solution for the Problem i on
-Day j of Advent of Code 2020.
-
-This is a template that I'll be using
-to solve problems.
+My solution for the Problem 1 on
+Day 12 of Advent of Code 2020.
 """
 
 from functools import reduce
@@ -12,27 +9,45 @@ import operator
 
 def process_group(grp):
     """
-    Given a group of tokens as a list,
-    compute whatever the problem asks
-    and return it.
+    Given a list of list of tokens
+    where each token starts with a
+    character from 'NSEWLRF', and
+    ends with a number, that describe
+    the motion of a ship and the direction
+    it faces at any given point in time,
+    compute the manhattan distance between
+    the initial point and the final point of
+    the ship.
 
-    Example
-    ___
-
-    # The problem A of Day 6.
-
-    return len(list(reduce(lambda x, y: x | y, map(set, grp))))
-
-    # The problem B of Day 6.
-
-    return len(list(reduce(lambda x, y: x & y, grp)))
+    :param grp: The list of list of tokens.
+    :return: The Manhattan distance between
+    the source and destination of the ship.
 
     """
 
-    # grp is a group of tokens.
-    # Compute whatever necessary.
+    dire = {"N": 0, "E": 0, "W": 0, "S": 0}
 
-    return len(grp)
+    ship_dir_options = ["E", "S", "W", "N"]
+
+    current_dir_idx = 0
+
+    for elem in grp:
+        c, num = elem[0], int(elem[1:])
+        if c == "R":
+            current_dir_idx = (current_dir_idx + num // 90) % 4
+        elif c == "L":
+            current_dir_idx = (current_dir_idx - num // 90) % 4
+        elif c == "F":
+            dire[ship_dir_options[current_dir_idx]] += num
+        else:
+            dire[c] += num
+
+    dist_ver = abs(dire["N"] - dire["S"])
+    dist_hor = abs(dire["E"] - dire["W"])
+
+    man_dist = dist_ver + dist_hor
+
+    return man_dist
 
 
 def reducer():
--- solutions/day_12_b.py
+++ solutions/day_12_b.py
@@ -1,9 +1,6 @@
 """
-My solution for the Problem i on
-Day j of Advent of Code 2020.
-
-This is a template that I'll be using
-to solve problems.
+My solution for the Problem 2 on
+Day 12 of Advent of Code 2020.
 """
 
 from functools import reduce
@@ -12,27 +9,105 @@ import operator
 
 def process_group(grp):
     """
-    Given a group of tokens as a list,
-    compute whatever the problem asks
-    and return it.
+    Given a list of list of tokens
+    where each token starts with a
+    character from 'NSEWLRF', and
+    ends with a number, that describe
+    the motion of a ship and a waypoint,
+    compute the manhattan distance between
+    the initial point and the final point of
+    the ship.
 
-    Example
-    ___
-
-    # The problem A of Day 6.
-
-    return len(list(reduce(lambda x, y: x | y, map(set, grp))))
-
-    # The problem B of Day 6.
-
-    return len(list(reduce(lambda x, y: x & y, grp)))
+    :param grp: The list of list of tokens
+    :return: The Manhattan distance between the
+    source and destination of the ship.
 
     """
 
-    # grp is a group of tokens.
-    # Compute whatever necessary.
+    dire = {"N": 0, "E": 0, "W": 0, "S": 0}
+    wpt = {"N": 1, "E": 10}
 
-    return len(grp)
+    def cw_90(x, y):
+        """
+        Given a coordinate (x, y)
+        compute the coordinate that is
+        rotated 90 degrees clock-wise
+        about the origin.
+
+        :param x: The x-coordinate
+        :param y: The y-coordinate
+
+        :return: A tuple (x, y) that
+        corresponds to a 90 degree
+        clock-wise rotation about the
+        origin.
+
+        """
+        return (y, -x)
+
+    def ccw_90(x, y):
+        """
+        Given a coordinate (x, y)
+        compute the coordinate that is
+        rotated 90 degrees counter
+        clock-wise about the origin.
+
+        :param x: The x-coordinate
+        :param y: The y-coordinate
+
+        :return: A tuple (x, y) that
+        corresponds to a 90 degree
+        counter clock-wise rotation
+        about the origin.
+
+        """
+        return (-y, x)
+
+    for elem in grp:
+        c, num = elem[0], int(elem[1:])
+
+        if c == "R":
+            for _ in range((num % 360) // 90):
+                nw_E, nw_N = cw_90(wpt["E"], wpt["N"])
+                wpt["E"], wpt["N"] = nw_E, nw_N
+
+        if c == "L":
+            for _ in range((num % 360) // 90):
+                nw_E, nw_N = ccw_90(wpt["E"], wpt["N"])
+                wpt["E"], wpt["N"] = nw_E, nw_N
+
+        elif c == "F":
+
+            tb_mag, lr_mag = abs(wpt["N"]), abs(wpt["E"])
+
+            top = wpt["N"] >= 0
+            right = wpt["E"] >= 0
+
+            if top:
+                dire["N"] += num * tb_mag
+            else:
+                dire["S"] += num * tb_mag
+            if right:
+                dire["E"] += num * lr_mag
+            else:
+                dire["W"] += num * lr_mag
+
+        else:
+            if c == "N":
+                wpt["N"] += num
+            elif c == "S":
+                wpt["N"] -= num
+            elif c == "W":
+                wpt["E"] -= num
+            elif c == "E":
+                wpt["E"] += num
+
+    dist_ver = abs(dire["N"] - dire["S"])
+    dist_hor = abs(dire["E"] - dire["W"])
+
+    man_dist = dist_ver + dist_hor
+
+    return man_dist
 
 
 def reducer():
