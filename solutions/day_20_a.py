2363a940eda18a14f0ef220e6ddeff0c1d91bb35
aalekhpatel07 <aalekh.gwpeck.7998@gmail.com>
2020-12-22 03:29:19 -0600

    Add solution, and tests for Day 20 of AoC 2020.

--- solutions/day_20_a.py
+++ solutions/day_20_a.py
@@ -9,22 +9,40 @@ from copy import deepcopy as dc
 
 
 def get_right_strip(chonk):
+    """
+    Compute the right vertical strip of a 2D list.
+    """
     return [chonk[_i][-1] for _i in range(len(chonk))]
 
 
 def get_top_strip(chonk):
+    """
+    Compute the top horizontal strip of a 2D list.
+    """
     return chonk[0]
 
 
 def get_left_strip(chonk):
+    """
+    Compute the left vertical strip of a 2D list.
+    """
     return [chonk[_i][0] for _i in range(len(chonk))]
 
 
 def get_bottom_strip(chonk):
+    """
+    Compute the bottom horizontal strip of a 2D list.
+    """
     return chonk[-1]
 
 
 def get_chonk(whole, i, j, n=12):
+    """
+    Given an index (i, j)
+    return a sublist of size 10 x 10
+    from the "whole" list.
+
+    """
     block_size = 10
 
     res = [[None for _ in range(block_size)] for _ in range(block_size)]
@@ -37,6 +55,11 @@ def get_chonk(whole, i, j, n=12):
 
 def fits(whole, chunk, i, j, n=12):
     """
+    Determine if the given chunk fits at
+    block (i, j) in the "whole" 2D list.
+
+    :return: True if its borders align
+    with its neighbors' borders, False otherwise.
 
     """
     _empty = ["." for _ in range(10)]
@@ -187,30 +210,54 @@ def fits(whole, chunk, i, j, n=12):
 
 
 def empty_chunk(n):
+    """
+    Produce a 2D list
+    of size n x n that
+    is populated with ".".
+    """
     return [["." for _ in range(n)] for _ in range(n)]
 
 
 def dfs(whole, row_idx, col_idx, n, tiles_assigned, mappings, used):
     """
+    DP the whole grid in the order left-to-right and top-to-bottom.
+    Where possible, place a tile, and if the end is reached,
+    we have a valid solution, otherwise it fails at some point
+    and then backtracks to a previous state.
+
+    :return: True if a valid tile assignment exists,
+    False otherwise.
+
+    Notes
+    _____
+
+    The "tiles_assigned" list is populated
+    if a solution is found. So we can inspect that
+    after this function finishes executing.
 
     """
 
+    # Reached the terminal state.
     if row_idx == n - 1 and col_idx == n:
-        # print('wtfgfhf')
         print(tiles_assigned)
         return True
+
+    # Hit the end of row. Go to next row and first column.
     elif col_idx == n:
         return dfs(whole, row_idx + 1, 0, n, tiles_assigned, mappings, used)
     else:
-        print(row_idx, col_idx)
+        # Check which tile id should fit at block (row_idx, col_idx).
         for k in mappings:
+            # Only worry about unused tiles,
+            # since we can use a tile only once.
             if k not in used:
                 for tile in mappings[k]:
+                    # If the tile fits fine:
                     if fits(whole, tile, row_idx, col_idx, n):
-
+                        # Place it.
                         whole = chunk_replace(whole, row_idx, col_idx, dc(tile))
+                        # Record the id of the tile at that index.
                         tiles_assigned[row_idx][col_idx] = k
-                        # pp(whole)
                         if dfs(
                             whole,
                             row_idx,
@@ -220,46 +267,42 @@ def dfs(whole, row_idx, col_idx, n, tiles_assigned, mappings, used):
                             mappings,
                             used | {k},
                         ):
+                            # This means, a valid assignment was found.
                             return True
+                        # Somewhere down the line, an index had no
+                        # good candidates for tiles. It made
+                        # the tile assignment bad.
+                        # So correct that by backtracking.
+
                         else:
+                            # Reset the tile assignment.
                             tiles_assigned[row_idx][col_idx] = -1
+                            # Replace the block with '.'.
                             whole = chunk_replace(
                                 whole, row_idx, col_idx, empty_chunk(10)
                             )
+        # All keys were used but no matching tile was found for this
+        # index, so return False.
         return False
 
 
-def temp_keys(res):
-    sw = {
-        1951: list("#...##.#.."),
-        2311: list("..###..###"),
-        3079: list("#.#.#####."),
-        2729: list("#.##...##."),
-        1427: list("..##.#..#."),
-        2473: list("..#.###..."),
-        2971: list("...#.#.#.#"),
-        1489: list("###.##.#.."),
-        1171: list(".##...####"),
-    }
-    temp = dict()
-    for k in res:
-        for v in res[k]:
-            if v[0] == sw[k]:
-                temp[k] = [v]
-                break
-    # print(len(temp))
-    return temp
-
-
 def pp(*tables):
+    """
+    A helper method
+    to simply print multiple
+    chunks of "." and "#" side-by-side.
+    """
     res = ""
     for zipped_row in zip(*tables):
         res += "\t".join(tuple("".join(x) for x in zipped_row)) + "\n"
     print(res)
-    return
 
 
 def parse_images(grp):
+    """
+    Read the input data into titles and
+    images.
+    """
     images = []
     titles = []
 
@@ -276,15 +319,12 @@ def solve(grp):
     from math import sqrt
 
     res = get_all_transforms(*parse_images(grp))
-    # res = temp_keys(res)
-    # print(res)
     _boxes = len(res)
     _sz = int(sqrt(_boxes))
     canvas = [["." for _ in range(_sz * 10)] for _ in range(_sz * 10)]
     used = set()
     tiles_assigned = [[-1 for _ in range(_sz)] for _ in range(_sz)]
     dfs(canvas, 0, 0, _sz, tiles_assigned, res, used)
-    # print(tiles_assigned)
     return (
         tiles_assigned[0][0]
         * tiles_assigned[0][-1]
@@ -295,6 +335,21 @@ def solve(grp):
 
 def get_all_transforms(titles, data):
     """
+    Given a list of titles and
+    tile data associated to each title,
+    compute a dictionary that has as keys
+    the unique titles and as values, a
+    list of rotations and flips of each tile
+    that is associated to each title.
+
+    :param titles: A list of tile ids.
+
+    :param data: A list of 2D lists that
+    represent blocks in the data.
+
+    :return: A dictionary of tile ids and
+    its corresponding collection of flips
+    and rotations.
 
     """
     res = dict()
@@ -306,6 +361,16 @@ def get_all_transforms(titles, data):
 
 def transform(arr):
     """
+    Given a 2D list,
+    rotate and flip it in
+    all possible ways and return
+    a collection of the views after
+    simulating such flips and
+    rotations.
+
+    :param arr: A 2D list
+    :return: A list of rotations
+    and flips of arr.
 
     """
     # One flip and 4 rotations.
@@ -328,7 +393,15 @@ def transform(arr):
 
 def rotate_right(arr):
     """
+    Rotate a copy of given 2D list
+    clockwise by 90 degrees
+    and return a new list.
 
+    :param arr: A 2D-list of arbitrary
+    dimensions.
+
+    :return: A list that is "arr" rotated
+    90 degree clockwise by its center.
     """
     n = len(arr)
     m = len(arr[0])
@@ -343,7 +416,7 @@ def rotate_right(arr):
 
 def flip_transpose(arr):
     """
-
+    Flip a 2D-list (i.e. transpose).
     """
     m = len(arr)
     n = len(arr[0])
@@ -357,13 +430,14 @@ def flip_transpose(arr):
 
 def chunk_replace(block, i, j, chunk):
     """
-
+    Replace the chunk of size 10 x 10
+    corresponding to a sublist indexed
+    by (i, j) in the larger list "block".
     """
     temp = dc(block)
     for x in range(len(chunk)):
         for y in range(len(chunk)):
             temp[10 * i + x][10 * j + y] = chunk[x][y]
-    # print('chonka:', temp)
     return temp
 
 
--- solutions/day_20_b.py
+++ solutions/day_20_b.py
@@ -1,91 +1,241 @@
 """
-My solution for the Problem i on
-Day j of Advent of Code 2020.
-
-This is a template that I'll be using
-to solve problems.
+My solution for the Problem 2 on
+Day 20 of Advent of Code 2020.
 """
 
+from pathlib import Path
+import os
 from functools import reduce
-import operator
+from itertools import product
+from copy import deepcopy as dc
 
 
-def process_group(grp):
+def parse_images(grp):
     """
-    Given a group of tokens as a list,
-    compute whatever the problem asks
-    and return it.
-
-    Example
-    ___
-
-    # The problem A of Day 6.
-
-    return len(list(reduce(lambda x, y: x | y, map(set, grp))))
-
-    # The problem B of Day 6.
-
-    return len(list(reduce(lambda x, y: x & y, grp)))
-
+    Read the input data into titles and
+    images.
     """
+    images = []
+    titles = []
 
-    # grp is a group of tokens.
-    # Compute whatever necessary.
+    for i in range(len(grp) // 11):
+        _title = int(grp[11 * i][:-1].split(" ")[1])
+        titles.append(_title)
+        images.append(grp[11 * i + 1 : 11 * (i + 1)])
+    return titles, images
 
-    return len(grp)
 
-
-def reducer():
+def get_all_transforms(titles, data):
     """
-    Define how to reduce the
-    groups.
+    Given a list of titles and
+    tile data associated to each title,
+    compute a dictionary that has as keys
+    the unique titles and as values, a
+    list of rotations and flips of each tile
+    that is associated to each title.
 
-    Example
-    ___
+    :param titles: A list of tile ids.
 
-    return lambda x, y: x + y
-    OR
-    return lambda x, y: x * y
-    OR
-    return operator.multiply
-    """
+    :param data: A list of 2D lists that
+    represent blocks in the data.
 
-    return operator.add
-
-
-# There's absolutely no need to touch any function below this line!
-# STOP!!!
-
-
-def solve(arr):
-    """
-    Given a list of lists
-    possibly separated by newlines,
-    'process' each group of lists
-    and reduce it to a result based
-    on the operator defined above.
-
-    :param arr: The list of list.
-    :return: The reduced map based on `operator()`.
+    :return: A dictionary of tile ids and
+    its corresponding collection of flips
+    and rotations.
 
     """
+    res = dict()
 
-    _i = 0
-    _group = []
-    group_results = []
+    for title, d in zip(titles, data):
+        res[title] = transform(d)
+    return res
 
-    while _i < len(arr):
-        if arr[_i] == "":
-            group_results.append(process_group(_group))
-            _group = []
-        else:
-            _group.append(arr[_i])
-        _i += 1
 
-    group_results.append(process_group(_group))
-    final_result = reduce(reducer(), group_results)
+def transform(arr):
+    """
+    Given a 2D list,
+    rotate and flip it in
+    all possible ways and return
+    a collection of the views after
+    simulating such flips and
+    rotations.
 
-    return final_result
+    :param arr: A 2D list
+    :return: A list of rotations
+    and flips of arr.
+
+    """
+    # One flip and 4 rotations.
+    res = []
+    curr = dc([list(x) for x in arr])
+    res.append(curr)
+
+    for _ in range(3):
+        curr = rotate_right(curr)
+        res.append(dc(curr))
+
+    curr = flip_transpose(arr)
+
+    for _ in range(4):
+        curr = rotate_right(curr)
+        res.append(dc(curr))
+
+    return res
+
+
+def rotate_right(arr):
+    """
+    Rotate a copy of given 2D list
+    clockwise by 90 degrees
+    and return a new list.
+
+    :param arr: A 2D-list of arbitrary
+    dimensions.
+
+    :return: A list that is "arr" rotated
+    90 degree clockwise by its center.
+    """
+    n = len(arr)
+    m = len(arr[0])
+
+    res = [[None for _ in range(n)] for _ in range(m)]
+    for i in range(m):
+        for j in range(n):
+            res[i][j] = arr[n - j - 1][i]
+
+    return res
+
+
+def flip_transpose(arr):
+    """
+    Flip a 2D-list (i.e. transpose).
+    """
+    m = len(arr)
+    n = len(arr[0])
+    res = [[-1 for _ in range(n)] for _ in range(m)]
+
+    for i in range(m):
+        for j in range(n):
+            res[i][j] = arr[j][i]
+    return res
+
+
+def remove_border(chonk):
+    """
+    Remove the borders of a 2D list.
+    """
+    res = []
+    for i in range(1, len(chonk) - 1):
+        temp = []
+        for j in range(1, len(chonk[i]) - 1):
+            temp.append(chonk[i][j])
+        res.append(dc(temp))
+    return res
+
+
+def count(grid, c):
+    """
+    Count the occurrences
+    of an object "c" in
+    the 2D list "grid".
+    """
+    acc = 0
+    for row in grid:
+        for elem in row:
+            acc += c == elem
+    return acc
+
+
+def find_monster(grid, mon):
+    """
+    Given a grid and a list of
+    indices of monster,
+    find the number of "#" that
+    are not part of any monster.
+
+    :param grid: An (8 * 12) x (8 * 12)
+    list of chars.
+    :param mon: A list of indices of
+    the monster.
+
+    :return: The count of "#" that
+    are not part of any sea monster.
+    """
+
+    monster_pounds = set()
+
+    for t in transform(grid):
+        # Monster is of height 2.
+        for i in range(len(grid) - 2):
+            # Monster is of width 19.
+            for j in range(len(grid[i]) - 19):
+
+                # Check all legal candidates for monsters.
+                cands = [grid[x + i][y + j] for x, y in mon]
+
+                # If monster found, update its set of indices.
+                if cands.count("#") == len(mon):
+                    monster_pounds |= {(x + i, y + j) for x, y in mon}
+
+    # Unique monster-free squares that are "#".
+    ans = count(grid, "#") - len(monster_pounds)
+    return ans
+
+
+def solve(grp):
+    """
+    Given some blocks of 10 x 10
+    tiles, compute the number of sea monsters
+    and yadi-yada.... See AoC (2020) Day 20.
+    """
+
+    # data computed from part a.
+    res = get_all_transforms(*parse_images(grp))
+
+    TOP = Path("data/input")
+
+    result = [[None for _ in range(12)] for _ in range(12)]
+
+    with open(TOP / "day_20_b_image.txt", "r") as f:
+        for line in f.readlines():
+            i, j, title, tile_idx = line.strip().split(" ")
+            result[int(i)][int(j)] = dc(res[int(title)][int(tile_idx)])
+
+    # Remove border.
+    no_border = [[None for _ in range(12)] for _ in range(12)]
+    for i in range(12):
+        for j in range(12):
+            no_border[i][j] = dc(remove_border(result[i][j]))
+
+    # Transform 12 x 12 x 8 x 8 into 96 x 96.
+    s_clean = [[None for _ in range(96)] for _ in range(96)]
+
+    for x in range(12):
+        for y in range(12):
+            for z in range(8):
+                for w in range(8):
+                    s_clean[8 * x + z][8 * y + w] = no_border[x][y][z][w]
+
+    # Indices of the sea monster.
+    sea_monster = [
+        (0, 18),
+        (1, 0),
+        (1, 5),
+        (1, 6),
+        (1, 11),
+        (1, 12),
+        (1, 17),
+        (1, 18),
+        (1, 19),
+        (2, 1),
+        (2, 4),
+        (2, 7),
+        (2, 10),
+        (2, 13),
+        (2, 16),
+    ]
+
+    return find_monster(dc(s_clean), sea_monster)
 
 
 def driver():
@@ -98,7 +248,9 @@ def driver():
     _n = int(input())
     arr = []
     for _ in range(_n):
-        arr.append(input())
+        temp = input()
+        if temp:
+            arr.append(temp)
 
     result = solve(arr)
     print(result)
