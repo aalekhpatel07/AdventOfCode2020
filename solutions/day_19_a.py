94775a9e179d36208d0cc19ac00bbab3cb86d387
aalekhpatel07 <aalekh.gwpeck.7998@gmail.com>
2020-12-25 07:24:33 -0600

    Add solution, and tests for Day 19 of AoC 2020.

--- solutions/day_19_a.py
+++ solutions/day_19_a.py
@@ -1,100 +1,95 @@
 """
-My solution for the Problem i on
-Day j of Advent of Code 2020.
-
-This is a template that I'll be using
-to solve problems.
+My solution for the Problem 1 on
+Day 19 of Advent of Code 2020.
 """
 
-from functools import reduce
 import operator
-from itertools import product
+from itertools import product as p
 
 
-def split(it, _f):
-    res = []
-    for elem in it:
-        if not _f(elem):
-            res.append(elem)
+def split_rules_and_strings(arr):
+    """
+    Given a list of strings,
+    representing the rules
+    and the query strings,
+    split the rules and strings
+    into a dictionary and a list
+    for further use.
+
+    :param arr: A list of strings.
+    :return: A tuple of dict and list
+    representing the ruleset and the
+    query strings.
+    """
+    rules = dict()
+    string_time = False
+    strings = []
+    for idx, elem in enumerate(arr):
+        if elem == "":
+            string_time = True
+        elif not string_time:
+            head, *rest = elem.split(": ")
+            rules[int(head)] = "".join(rest)
         else:
-            yield res[:]
-            res = []
-    yield res
+            strings.append(elem)
+    return rules, strings
 
 
-def parse_rules(rules):
-    adj_list = list(map(lambda x: tuple(x.split(":")), rules))
-    adj = dict()
-
-    for v, rule_v in adj_list:
-        adj[int(v)] = tuple(rule_v.split("|"))
-    memo = dict()
-    curr = 0
-    for k in adj:
-        temp = adj[k]
-        chars = []
-        for t in temp:
-            if t.strip() == '"a"' or t.strip() == '"b"':
-                chars.append(t.strip().replace('"', ""))
-            else:
-                chars.append(list(map(int, t.strip().split(" ")[:])))
-        adj[k] = chars[:]
-
-    _, memo = generate_rules(adj, curr, memo)
-
-    # memo[0] = []
-    # for group in adj[0]:
-    #     for entries in product(*[memo[z] for z in group]):
-    #         memo[0].append("".join(entries))
-    return memo
+_DEPTH_MAX = 20
 
 
-def generate_rules(adj, curr, memo):
-    if len(adj[curr]) == 1 and isinstance(adj[curr][0], str):
-        memo[curr] = adj[curr]
-        return memo[curr], memo
-    elif curr in memo:
-        return memo[curr], memo
+def generate_rules(rules, current, depth=0):
+    """
+    Generate all the strings that
+    can be produced from a given ruleset.
+
+    :param rules: A dict with keys as ints,
+    and values as a string representing a rule.
+    :param current: An int, representing the current
+    ruleset under consideration.
+    :param depth: A helper parameter that stops
+    unnecessary recursion if there is a loop in the ruleset.
+
+    :yield: An str object that matches the ruleset defined
+    for "current".
+    """
+    if depth > _DEPTH_MAX:
+        # Too deep. Probably a loop exists.
+        return
+    elif rules[current] in ('"a"', '"b"'):
+        # Base cases.
+        yield rules[current][1:2]
     else:
-        for group in adj[curr]:
-            # print(curr, group)
-            for entry in group:
-                _, memo = generate_rules(adj, entry, memo)
-        if curr not in memo:
-            memo[curr] = []
-        for group in adj[curr]:
-            temp = [memo[z] for z in group]
-            for cols in product(*temp):
-                memo[curr].append("".join(cols))
-    return _, memo
+        # For each group we must union the set of strings formed.
+        for group in rules[current].split(" | "):
+            # Combine the solution from children and yield
+            # all possible strings that match.
+            for cmb in p(
+                *[generate_rules(rules, int(m), depth + 1) for m in group.split()]
+            ):
+                yield "".join(cmb)
 
 
 def solve(grp):
     """
+    Given a list of strings,
+    representing the ruleset
+    and query strings,
+    solve the problem of AoC
+    2020 (day 19).
+
+    :param grp: A list of str.
+    :return: The count of query
+    strings that match to ruleset 0.
     """
 
-    def ssplit(it, _f):
-        res = []
-        for elem in it:
-            if not _f(elem):
-                res.append(elem)
-            else:
-                yield res[:]
-                res = []
-        yield res
-
-    rules, strings = list(ssplit(grp, lambda x: x == ""))
-    memo = parse_rules(rules)
-    zero = memo[0]
-    print("memo computing done!")
-    print(0 in memo)
-    print(len(memo[0]), len(strings))
-    count = 0
-    for m in strings:
-        if any(x == m for x in zero):
-            count += 1
-
-    return count
+    rules, strings = split_rules_and_strings(grp)
+    zero = set(generate_rules(rules, 0))
+    res = 0
+    for s in strings:
+        if s in zero:
+            res += 1
+    return res
 
 
 def reducer():
@@ -121,7 +116,6 @@ def driver():
     :return: result - Result of computation.
 
     """
-
     _n = int(input())
     arr = []
     for _ in range(_n):
